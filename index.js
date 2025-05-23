ad4d83048a4443533e1bb5cfc2fc8be7d95d54f6
Pygramer78 <ayuskyap@gmail.com>
2025-05-23 19:06:31 +0200

    Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  });
+
+  // Carga el HTML desde tu carpeta /html
+  win.loadFile(path.join(__dirname, 'html', 'Wiki_Games.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+const p = document.getElementById("p");
+
+function change(page) {
+    window.location.href = page;
+}
